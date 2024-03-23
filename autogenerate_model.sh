d7a5c908eaa404389ffd7fd46fb7ef9c4694a19b
janotama <putrajanotama1@gmail.com>
2024-03-23 20:28:55 +0800

    re init

--- autogenerate_model.sh
+++
@@ -1,7 +0,0 @@
-database_name="svassa"
-host_name="127.0.0.1"
-user_name="root"
-port_number="3306"
-password=""
-
-sequelize-auto -o "./models" -d $database_name -h $host_name -u $user_name -p $port_number -e mysql 
